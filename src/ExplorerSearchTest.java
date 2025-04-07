037ee121994501ac228752962d3c06198a625a0a
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-04-07 08:32:54 -0700

    Created a few basic test cases. Still trying to come up with some more!

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -17,4 +17,49 @@ public class ExplorerSearchTest {
 
     // Add more tests here!
     // Come up with varied cases
+    
+    // Test where everywhere is walkable
+    @Test
+    public void testReachableArea_allAreasAreWalkable() {
+        int[][] island = {
+            {0,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+            {1,1,1,1,1,1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(30, actual);
+    }
+
+    // Test where there is nowhere to go
+    @Test
+    public void testReachableArea_nowhereToGo() {
+        int[][] island = {
+            {3,3,3,3,3,3},
+            {3,2,3,3,3,3},
+            {3,2,2,3,3,3},
+            {3,2,2,2,2,2},
+            {3,2,2,0,2,3}
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(1, actual);
+    }
+
+    // Test for No Starting point
+    @Test
+    public void testReachableArea_noStartingPoint() {
+        int[][] island = {
+            {1,2,3,2,3,3},
+            {1,2,3,2,3,3},
+            {1,2,3,2,3,3},
+            {1,2,3,2,3,3},
+            {1,2,3,2,3,3}
+        };
+        
+        assertThrows(IllegalArgumentException.class, () -> {
+            ExplorerSearch.reachableArea(island);
+        });
+    }
+
 }
